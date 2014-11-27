f8bf6c56dc5a8b7903413d7bffd9f0ff02c787e6
stfnjstn <stfn.jstn@gmail.com>
2014-11-27 22:11:08 +0100

    Paused Handling

--- MySecondGame/GameScene.swift
+++ MySecondGame/GameScene.swift
@@ -23,6 +23,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var remainingLifes = 3
     var scoreNode = SKLabelNode()
     var score = 0
+    var gamePaused = false
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
@@ -70,35 +71,47 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            
-            // Determine the new position for the invisible sprite:
-            // The calculation is needed to ensure the positions of both sprites
-            // are nearly the same, but different. Otherwise the hero sprite rotates
-            // back to it's original orientation after reaching the location of
-            // the invisible sprite
-            var xOffset:CGFloat = 1.0
-            var yOffset:CGFloat = 1.0
             var location = touch.locationInNode(self)
-            if location.x>heroSprite.position.x {
-                xOffset = -1.0
+            var node = self.nodeAtPoint(location)
+            if (node.name == "PauseButton") || (node.name == "PauseButtonContainer") {
+                showPauseAlert()
+            } else {
+       
+                // Determine the new position for the invisible sprite:
+                // The calculation is needed to ensure the positions of both sprites
+                // are nearly the same, but different. Otherwise the hero sprite rotates
+                // back to it's original orientation after reaching the location of
+                // the invisible sprite
+                var xOffset:CGFloat = 1.0
+                var yOffset:CGFloat = 1.0
+                if location.x>heroSprite.position.x {
+                    xOffset = -1.0
+                }
+                if location.y>heroSprite.position.y {
+                    yOffset = -1.0
+                }
+            
+                // Create an action to move the invisibleControllerSprite.
+                // This will cause automatic orientation changes for the hero sprite
+                let actionMoveInvisibleNode = SKAction.moveTo(CGPointMake(location.x - xOffset, location.y - yOffset), duration: 0.2)
+                invisibleControllerSprite.runAction(actionMoveInvisibleNode)
+            
+                // Create an action to move the hero sprite to the touch location
+                let actionMove = SKAction.moveTo(location, duration: 1)
+                heroSprite.runAction(actionMove)
             }
-            if location.y>heroSprite.position.y {
-                yOffset = -1.0
-            }
-            
-            // Create an action to move the invisibleControllerSprite.
-            // This will cause automatic orientation changes for the hero sprite
-            let actionMoveInvisibleNode = SKAction.moveTo(CGPointMake(location.x - xOffset, location.y - yOffset), duration: 0.2)
-            invisibleControllerSprite.runAction(actionMoveInvisibleNode)
-            
-            // Create an action to move the hero sprite to the touch location
-            let actionMove = SKAction.moveTo(location, duration: 1)
-            heroSprite.runAction(actionMove)
-            
         }
     }
     
-    
+    // Show Pause Alert
+    func showPauseAlert() {
+        self.gamePaused = true
+        var alert = UIAlertController(title: "Pause", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Continue", style: UIAlertActionStyle.Default)  { _ in
+            self.gamePaused = false
+            })
+        self.view?.window?.rootViewController?.presentViewController(alert, animated: true, completion: nil)
+    }
 
     
     func createHUD() {
@@ -149,17 +162,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
    
     func didBeginContact(contact: SKPhysicsContact) {
-    
+        if !self.gamePaused {
+        
+        }
     }
     
     var _dLastShootTime: CFTimeInterval = 1
     
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+
+        if !self.gamePaused {
             
-        if currentTime - _dLastShootTime >= 1 {
-            enemySprites.shoot(heroSprite)
-            _dLastShootTime=currentTime
+            if currentTime - _dLastShootTime >= 1 {
+                enemySprites.shoot(heroSprite)
+                _dLastShootTime=currentTime
+            }
         }
     }
 
